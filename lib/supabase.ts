73726e3dfc87491a257ce49f6e8a7a65f1099259
공대적 문과생 <moonlygreat@gmail.com>
2025-08-12 22:36:54 +0900

    chore: remove leading newline

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,6 @@
-
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://kmdtswcayohamrxbgptf.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImttZHRzd2NheW9oYW1yeGJncHRmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQzNzQ5OTAsImV4cCI6MjA2OTk1MDk5MH0.Gtkq8_bpDOiUeXDQPdkrxtLR2FNtrZlmoTka1x_9KaA';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+);
